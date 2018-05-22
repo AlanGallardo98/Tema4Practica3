c943c6fd2136697439db4eb3b6e384c97c4dc64f
Alan <alangallardoclemente@hotmail.com>
2018-05-22 09:05:45 +0200

    Commit de src/Clase1

--- src/Clase1.java
+++ src/Clase1.java
@@ -1,8 +1,28 @@
 
 public class Clase1
 {
-	// Atributo
+	// Atributos
 	
 	private int variable1;
 	private int variable2;
+	
+	// Getters & Setters
+	
+	public int getVariable1()
+	{
+		return variable1;
+	}
+	public void setVariable1(int variable1)
+	{
+		this.variable1 = variable1;
+	}
+	public int getVariable2()
+	{
+		return variable2;
+	}
+	public void setVariable2(int variable2)
+	{
+		this.variable2 = variable2;
+	}
+
 }
